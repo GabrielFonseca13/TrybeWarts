bacaf4d82f6bbcf4ccc17498531e7d54bbd28852
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-07 16:39:04 -0300

    terminando o requisito 3 e corrigindo os erros de Lint

---
+++ script.js
@@ -0,0 +1,12 @@
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const botao = document.querySelector('#botao');
+
+function teste() {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+botao.addEventListener('click', teste);
